42cefe1419aef3223a129fe34ebea91a62e13740
Alhassan-Ali7 <132752068+Alhassan-Ali7@users.noreply.github.com>
2023-06-13 02:39:02 +0300

    best seller list view item part one

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentaion/views/widgets/fetured_list_view.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -18,10 +19,39 @@ class HomeViewBody extends StatelessWidget {
           child: Text(
             'Best Seller',
             style: Styles.titleMedium,
-            
           ),
         ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30.0),
+      child: SizedBox(
+        height: 105,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2 / 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.book),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,4 +3,6 @@ class AssetsData{
   static const logo = 'assets/images/Logo.png';
 
   static const testImage = 'assets/images/test_image.png';
+
+  static const book = 'assets/images/book.png';
 }
